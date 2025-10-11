e9113397d9811bf59d78cdb4c9091050fbfb36af
blourvim <blourvim@gmail.com>
2025-10-12 02:22:49 +0300

    add prints

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ use hostprint::{connection::ssh, model::host::Host};
 
 fn main() {
     let my_host: Host = Host::new();
-    let mut ssh_command = ssh::SSHClient::new("localhost")
+    let mut ssh_command = ssh::SSHClient::new("127.0.0.1")
         .with_private_key("./id_rsa")
         .with_command("uname -a")
+        .with_port(3000u16)
+        .with_username("devel")
         .build();
 
     let res = ssh_command.output().expect("error");
-    println!("{:?}",res.stdout);
+    println!("stdout{:?}",str::from_utf8(&res.stdout).unwrap());
+    println!("status{:?}",res.status);
+    println!("stderr{:?}",str::from_utf8(&res.stderr).unwrap());
 }
