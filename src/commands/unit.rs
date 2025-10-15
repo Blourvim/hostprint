bbe5a1997d168108942becb88c7ff93707ab7ef2
blourvim <blourvim@gmail.com>
2025-10-15 20:28:40 +0300

    define command unti

--- src/commands/unit.rs
+++ src/commands/unit.rs
@@ -1,3 +1,8 @@
-pub struct Unit{
-    name:String,
+use crate::model::host::Host;
+
+pub struct Unit {
+    name: String,
+    comand: String,
+    parser: fn(stdout: &str, stderr: &str, &mut Host) -> Host,
+    follow_up: fn(stdout: &str, stderr: &str, &mut Host) -> (),
 }
