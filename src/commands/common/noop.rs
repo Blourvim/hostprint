62a645e080402fa1089036222cd4f7a0f67ac3fb
blourvim <blourvim@gmail.com>
2025-11-01 20:27:35 +0300

    add verbosity

--- src/commands/common/noop.rs
+++ src/commands/common/noop.rs
@@ -1,8 +1,14 @@
+use std::env;
 use crate::model::host::Host;
 
 pub fn noop_follow_up(stdout: &str, stderr: &str, _host: &mut Host) {
-    println!("Command output:\n{}", stdout);
-    if !stderr.trim().is_empty() {
-        eprintln!("stderr: {}", stderr);
+    // Check if "--verbose" or "-v" exists in the command-line arguments
+    let verbose = env::args().any(|arg| arg == "--verbose" || arg == "-v");
+
+    if verbose {
+        println!("Command output:\n{}", stdout);
+        if !stderr.trim().is_empty() {
+            eprintln!("stderr: {}", stderr);
+        }
     }
 }
