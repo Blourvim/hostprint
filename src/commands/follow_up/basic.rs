3ef2ad74dcbf46829b72b12e23797078ea22ef16
blourvim <blourvim@gmail.com>
2025-11-06 17:11:58 +0300

    clear individual logs

--- src/commands/follow_up/basic.rs
+++ src/commands/follow_up/basic.rs
@@ -3,50 +3,43 @@ use crate::model::{
         df::DfFacts, du::DuFacts, id::IdFacts, os_release::OsReleaseFacts,
         passwd::GetentPasswdFacts, ss::SsFacts, uname::UnameFacts, uptime::UptimeFacts, w::WFacts,
     },
-    host::Host,
+    host::{Host, Os},
 };
 
 pub fn uname_follow_up(stdout: &str, stderr: &str, host: &mut Host) -> () {
     let facts = UnameFacts::new(stdout.into());
-    println!("{:?}", facts)
+
+    host.os =
 }
 
 pub fn os_release_follow_up(stdout: &str, stderr: &str, host: &mut Host) -> () {
     let facts = OsReleaseFacts::new(stdout.into());
-    println!("{:?}", facts)
 }
 
 pub fn getent_passwd_follow_up(stdout: &str, stderr: &str, host: &mut Host) -> () {
     let facts = GetentPasswdFacts::from_getent(stdout.into());
-    println!("{:?}", facts)
 }
 
 pub fn id_followup(stdout: &str, stderr: &str, host: &mut Host) -> () {
     let facts = IdFacts::from_std(stdout.into());
-    println!("{:?}", facts)
 }
 
 pub fn uptime_followup(stdout: &str, stderr: &str, host: &mut Host) -> () {
     let facts = UptimeFacts::from_std(stdout.into());
-    println!("{:?}", facts)
 }
 
 pub fn w_followup(stdout: &str, stderr: &str, host: &mut Host) -> () {
     let facts = WFacts::from_std(stdout.into());
-    println!("{:?}", facts)
 }
 
 pub fn df_followup(stdout: &str, stderr: &str, host: &mut Host) -> () {
     let facts = DfFacts::from_std(stdout.into());
-    println!("{:?}", facts)
 }
 
 pub fn du_followup(stdout: &str, stderr: &str, host: &mut Host) -> () {
     let facts = DuFacts::from_std(stdout.into());
-    println!("{:?}", facts)
 }
 
 pub fn ss_followup(stdout: &str, stderr: &str, host: &mut Host) -> () {
     let facts = SsFacts::from_std(stdout.into());
-    println!("{:?}", facts)
 }
--- src/model/host.rs
+++ src/model/host.rs
@@ -2,14 +2,14 @@ use crate::model::hardware::hardware::Hardware;
 use std::{collections::HashSet, default, u64};
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
-enum Os {
+pub enum Os {
     Arch(String),
     Debian(String),
     Other(String),
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
-enum PackageManagers {
+pub enum PackageManagers {
     Pacman(String),
     Apt(String),
     Pamac(String),
@@ -17,21 +17,21 @@ enum PackageManagers {
     Other(String),
 }
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone, Hash, PartialEq, Eq)]
-struct Group {
-    pid: Option<u64>,
-    name: Option<u64>,
+pub struct Group {
+    pub pid: Option<u64>,
+    pub name: Option<u64>,
 }
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone, PartialEq, Eq)]
-struct SystemUser {
-    pid: Option<u64>,
-    groups: Option<HashSet<Group>>,
+pub struct SystemUser {
+    pub pid: Option<u64>,
+    pub groups: Option<HashSet<Group>>,
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct Host {
     // current_user: SystemUser,
-    os: Option<Os>,
-    hardware: Option<Hardware>,
+    pub os: Option<Os>,
+    pub hardware: Option<Hardware>,
     // package_managers: HashSet<PackageManagers>,
 }
 
