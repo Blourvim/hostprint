8f0605945ef7a854be418c0bc6063c1bd0f8dc23
blourvim <blourvim@gmail.com>
2025-11-07 16:51:37 +0300

    init dasboard serve

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::env;
+use std::{env, net::TcpListener};
 
 use hostprint::{
     commands::{basic, firewall, hardware, package, services},
     connection::ssh::SSHClient,
     model::host::Host,
 };
+use std::io::{BufRead, Write};
 fn get_arg_value(flag: &str) -> Option<String> {
     let mut args = env::args();
     while let Some(arg) = args.next() {
@@ -43,6 +44,25 @@ fn main() -> std::io::Result<()> {
         println!("\n=== {} ===", unit.name);
         let stdout = shell.exec(&unit.command)?;
         (unit.follow_up)(&stdout, "", &mut host);
+    }
+
+    if let Some(serve_port) = get_arg_value("--serve") {
+
+        const PAGE: &str = include_str!("./view/template/index.html");
+        let listener = std::net::TcpListener::bind(&serve_port).unwrap();
+
+        println!("Dashboard is available at{:?}", &serve_port);
+        for mut stream in listener.incoming().flatten() {
+            let mut reader = std::io::BufReader::new(&mut stream);
+            let mut l = String::new();
+            reader.read_line(&mut l).unwrap();
+            stream.write_all(b"HTTP/1.1 200 OK\r\n").unwrap();
+            stream
+                .write_all(b"Content-Type: text/html; charset=UTF-8\r\n")
+                .unwrap();
+            stream.write_all(b"\r\n").unwrap();
+            stream.write_all(PAGE.as_bytes()).unwrap();
+        }
     }
     Ok(())
 }
---
+++ src/view/template/dashboard.rs
@@ -0,0 +1,5 @@
+use crate::model::host::Host;
+
+pub fn generate_dashboard(host: Host) -> String {
+    return r"<h1>Hello world </h1>".into();
+}
