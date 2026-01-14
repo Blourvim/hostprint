fad295039cfea4502df6391cb13167bea0f49236
blourvim <blourvim@gmail.com>
2026-01-14 04:19:37 +0300

    init files for renderers for the md format

---
+++ src/view/md/renderers/mod.rs
@@ -0,0 +1,11 @@
+pub mod metrics;
+pub mod hardware;
+pub mod users;
+pub mod sockets;
+pub mod sesssions;
+pub mod packages;
+pub mod package_managers;
+pub mod os;
+pub mod groups;
+pub mod current_user;
+
