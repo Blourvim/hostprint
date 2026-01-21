fbd58155ed3cd42fbea9170c158fa39f3417830d
blourvim <blourvim@gmail.com>
2026-01-21 18:38:50 +0300

    implement rendering strategies

--- src/view/md/md.rs
+++ src/view/md/md.rs
@@ -2,7 +2,11 @@ use std::fmt::Write;
 
 use crate::{
     model::host::Host,
-    view::md::renderers::{self, current_user::generate_current_user_section, groups::generate_groups_section, metrics::generate_metrics_section},
+    view::md::renderers::{
+        current_user::generate_current_user_section, groups::generate_groups_section,
+        metrics::generate_metrics_section, os::generate_os_section,
+        sesssions::generate_active_sessions_section, sockets::generate_sockets_section,
+    },
 };
 
 pub struct Md {
@@ -23,11 +27,12 @@ impl Md {
     fn generate_markdown(content: &mut String, host: &Host) {
         writeln!(content, "# Host System Information\n").unwrap();
         writeln!(content, "*Generated from system inventory data*\n").unwrap();
-
         generate_current_user_section(content, &host.current_user);
         generate_groups_section(content, &host.groups);
         generate_metrics_section(content, &host.metrics);
-
+        generate_os_section(content, &host.os);
+        generate_active_sessions_section(content, &host.sessions);
+        generate_sockets_section(content, &host.sockets);
         println!("{:?}", content)
     }
 }
