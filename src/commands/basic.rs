da369a4afcfec66243a7e97f26953aba97b84a05
blourvim <blourvim@gmail.com>
2026-01-22 14:27:45 +0300

    remove comments, clear code

--- src/commands/basic.rs
+++ src/commands/basic.rs
@@ -4,6 +4,7 @@ use crate::commands::{
         basic::{
             df_follow_up, du_follow_up, getent_passwd_follow_up, groups_follow_up, id_follow_up,
             os_release_follow_up, ss_follow_up, uname_follow_up, uptime_follow_up, w_follow_up,
+            whoami_follow_up,
         },
         network::network_interfaces_follow_up,
     },
@@ -34,40 +35,13 @@ pub fn default_units() -> Vec<Unit> {
         Unit::new("Uptime", "uptime", uptime_follow_up),
         // TODO w for containers out of scope for now
         Unit::new("Logged-in Users", "w -h ", w_follow_up),
-
         Unit::new("Open Ports", "ss -HtulnpO", ss_follow_up),
-        Unit::new("Network Interfaces", "ip addr", network_interfaces_follow_up),
-        //  OUT OF SCOPE
-        //  -----------------------------------
-        // Unit::new(
-        //     "Running Services",
-        //     "systemctl list-units --type=service --state=running",
-        //     noop_follow_up,
-        // ),
-        // Unit::new(
-        //     "Top Memory Processes",
-        //     "ps aux --sort=-%mem | head -15",
-        //     noop_follow_up,
-        // ),
-        //Unit::new("Memory", "free -h", noop_follow_up),
-        //Unit::new("Top Processes", "top -n 1 | head -20", noop_follow_up),
-        //Unit::new("Home Directories", "ls -lah /home", noop_follow_up),
-        //Unit::new("Web Roots", "ls -lah /var/www", noop_follow_up),
-        //Unit::new("Opt Folders", "ls -lah /opt", noop_follow_up),
-        //Unit::new("Logs", "ls -lah /var/log", noop_follow_up),
-        //Unit::new(
-        //    "Package Manager",
-        //    "which apt || which yum || which dnf || which pacman",
-        //    noop_follow_up,
-        //),
-        //Unit::new("Packages", "dpkg -l | head -20", noop_follow_up),
-        //Unit::new("Cron Directory", "ls -lah /etc/cron*", noop_follow_up),
-        // TODO operations which require elevated permissions are out of scope for now
-        // Unit::new("Cron Jobs", "sudo crontab -l", noop_follow_up),
-        // Unit::new("Recent Syslog", "sudo tail -n 20 /var/log/syslog 2>/dev/null || sudo tail -n 20 /var/log/messages", noop_follow_up),
-
-        // already implemented another comand for it unnecesary for now
-        Unit::new("Current User", "whoami", noop_follow_up),
+        Unit::new(
+            "Network Interfaces",
+            "ip addr",
+            network_interfaces_follow_up,
+        ),
+        Unit::new("Current User", "whoami", whoami_follow_up),
         Unit::new("Groups", "groups", groups_follow_up),
     ];
 }
